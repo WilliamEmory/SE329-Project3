5440f022453401da2896867f7d76cc07c4e82152
KimSungTak <dsk@iastate.edu>
2015-03-28 14:34:38 -0500

    trying to get jquery to work

---
+++ scripts/popup.js
@@ -0,0 +1,34 @@
+//https://github.com/dburles/chrome-tab-popup trying to get jquery to work
+var ellipsize = function(string) {
+  var length = 40;
+  if (string.length > length)
+    return string.substr(0, length) + '&hellip;';
+  return string;
+};
+
+$(function() {
+  chrome.tabs.query({ currentWindow: true }, function(tab) {
+    for (var i = tab.length - 1; i >= 0; i--) {
+      var currentTab = tab[i];
+      
+      $('ul').append(
+        '<li id="' + currentTab.id + '">' +
+          '<img width="25" height="25" src="' + currentTab.favIconUrl + '">' +
+          '<a>' + ellipsize(currentTab.title) + '</a>' +
+          '<span class="close">x</span>' +
+        '</li>');
+    }
+    $('.tab-count').text(tab.length);
+  });
+
+  $('body').on('click', 'li', function() {
+    var tabId = parseInt($(this).attr('id'), 10);
+    chrome.tabs.update(tabId, { active: true });
+  });
+
+  $('body').on('click', '.close', function() {
+    var parent = $(this).parent();
+    chrome.tabs.remove(parseInt(parent.attr('id'), 10));
+    parent.remove();
+  });
+});
